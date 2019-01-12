8a54b05e326388229125ffe6baaa58379a631323
Daniel <daniel@supersmart.co.il>
2019-01-12 13:42:33 +0200

    NEW - track service

---
+++ app/src/main/java/com/trackyourself/AddLocationActivity.java
@@ -0,0 +1,5 @@
+package com.trackyourself;
+
+public class AddLocationActivity {
+
+}
---
+++ app/src/main/java/com/trackyourself/TrackingService.java
@@ -0,0 +1,153 @@
+package com.trackyourself;
+
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import android.annotation.SuppressLint;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.support.v4.content.ContextCompat;
+import android.os.IBinder;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.util.Log;
+import android.Manifest;
+import android.location.Location;
+import android.app.Notification;
+import android.content.pm.PackageManager;
+import android.app.PendingIntent;
+import android.app.Service;
+
+import java.util.concurrent.Executor;
+
+public class TrackingService extends Service implements OnSuccessListener {
+
+    private static final String TAG = TrackingService.class.getSimpleName();
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+       // buildNotification();
+        loginToFirebase();
+    }
+
+//Create the persistent notification//
+
+    @SuppressLint("NewApi")
+    private void buildNotification() {
+        String stop = "stop";
+        registerReceiver(stopReceiver, new IntentFilter(stop));
+        PendingIntent broadcastIntent = PendingIntent.getBroadcast(
+                this, 0, new Intent(stop), PendingIntent.FLAG_UPDATE_CURRENT);
+
+// Create the persistent notification//
+        Notification.Builder builder = new Notification.Builder(this)
+                .setContentTitle(getString(R.string.app_name))
+                .setContentText(getString(R.string.tracking_enabled_notif))
+                .setOngoing(true)
+                .setContentIntent(broadcastIntent)
+                .setSmallIcon(R.drawable.tracking_enabled);
+        startForeground(1, builder.build());
+    }
+
+    protected BroadcastReceiver stopReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+
+//Unregister the BroadcastReceiver when the notification is tapped//
+
+            unregisterReceiver(stopReceiver);
+
+//Stop the Service//
+
+            stopSelf();
+        }
+    };
+
+    private void loginToFirebase() {
+
+//Authenticate with Firebase, using the email and password we created earlier//
+
+        String email = getString(R.string.test_email);
+        String password = getString(R.string.test_password);
+
+//Call OnCompleteListener if the user is signed in successfully//
+
+        FirebaseAuth.getInstance().signInWithEmailAndPassword(
+                email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(Task<AuthResult> task) {
+
+//If the user has been authenticated...//
+
+                if (task.isSuccessful()) {
+
+//...then call requestLocationUpdates//
+
+                    requestLocationUpdates();
+                } else {
+
+//If sign in fails, then log the error//
+
+                    Log.d(TAG, "Firebase authentication failed");
+                }
+            }
+        });
+    }
+
+//Initiate the request to track the device's location//
+
+    private void requestLocationUpdates() {
+        LocationRequest request = new LocationRequest();
+
+//Specify how often your app should request the device’s location//
+
+        request.setInterval(10000);
+
+//Get the most accurate location data available//
+
+        request.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        FusedLocationProviderClient client = LocationServices.getFusedLocationProviderClient(this);
+        int permission = ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION);
+
+//If the app currently has access to the location permission...//
+
+        if (permission == PackageManager.PERMISSION_GRANTED) {
+            client.getLastLocation().addOnSuccessListener(this);
+        }
+
+    }
+
+
+
+    @Override
+    public void onSuccess(Object o) {
+        final String path = getString(R.string.firebase_path);
+
+        Location location = (Location) o;
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference(path);
+
+        // Got last known location. In some rare situations this can be null.
+        if (location != null) {
+            // Logic to handle location object
+            ref.setValue(location);
+        }
+    }
+}
